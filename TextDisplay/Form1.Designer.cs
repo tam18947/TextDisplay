b51a4a7d54c754dc2220eaf8c311b5befb815a9d
sasaki <sasakikei005@gmail.com>
2022-06-02 00:00:06 +0900

    プロジェクト ファイルを追加します。

---
+++ TextDisplay/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace TextDisplay
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.foreColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.snapAssistToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.topmostToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.textToolStripMenuItem,
+            this.fontToolStripMenuItem,
+            this.foreColorToolStripMenuItem,
+            this.backgroundColorToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.snapAssistToolStripMenuItem,
+            this.topmostToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.closeToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(177, 170);
+            // 
+            // textToolStripMenuItem
+            // 
+            this.textToolStripMenuItem.Name = "textToolStripMenuItem";
+            this.textToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.textToolStripMenuItem.Text = "Text...";
+            this.textToolStripMenuItem.Click += new System.EventHandler(this.TextToolStripMenuItem_Click);
+            // 
+            // fontToolStripMenuItem
+            // 
+            this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            this.fontToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.fontToolStripMenuItem.Text = "Font...";
+            this.fontToolStripMenuItem.Click += new System.EventHandler(this.FontToolStripMenuItem_Click);
+            // 
+            // foreColorToolStripMenuItem
+            // 
+            this.foreColorToolStripMenuItem.Name = "foreColorToolStripMenuItem";
+            this.foreColorToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.foreColorToolStripMenuItem.Text = "Text color...";
+            this.foreColorToolStripMenuItem.Click += new System.EventHandler(this.ForeColorToolStripMenuItem_Click);
+            // 
+            // backgroundColorToolStripMenuItem
+            // 
+            this.backgroundColorToolStripMenuItem.Name = "backgroundColorToolStripMenuItem";
+            this.backgroundColorToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.backgroundColorToolStripMenuItem.Text = "Background color...";
+            this.backgroundColorToolStripMenuItem.Click += new System.EventHandler(this.BackColorToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(173, 6);
+            // 
+            // snapAssistToolStripMenuItem
+            // 
+            this.snapAssistToolStripMenuItem.Checked = true;
+            this.snapAssistToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.snapAssistToolStripMenuItem.Name = "snapAssistToolStripMenuItem";
+            this.snapAssistToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.snapAssistToolStripMenuItem.Text = "Snap assist";
+            this.snapAssistToolStripMenuItem.Click += new System.EventHandler(this.SnapAssistToolStripMenuItem_Click);
+            // 
+            // topmostToolStripMenuItem
+            // 
+            this.topmostToolStripMenuItem.Checked = true;
+            this.topmostToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.topmostToolStripMenuItem.Name = "topmostToolStripMenuItem";
+            this.topmostToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.topmostToolStripMenuItem.Text = "Topmost";
+            this.topmostToolStripMenuItem.Click += new System.EventHandler(this.TopmostToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(173, 6);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.closeToolStripMenuItem.Text = "Close(&C)";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.CloseToolStripMenuItem_Click);
+            // 
+            // fontDialog1
+            // 
+            this.fontDialog1.ShowColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ContextMenuStrip = this.contextMenuStrip1;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.GhostWhite;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(470, 45);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Right click to edit this message.";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Text display";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem snapAssistToolStripMenuItem;
+        private ToolStripMenuItem closeToolStripMenuItem;
+        private ToolStripMenuItem topmostToolStripMenuItem;
+        private ToolStripMenuItem fontToolStripMenuItem;
+        private FontDialog fontDialog1;
+        private ToolStripMenuItem backgroundColorToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator2;
+        private ToolStripSeparator toolStripSeparator1;
+        private ColorDialog colorDialog1;
+        private ToolStripMenuItem foreColorToolStripMenuItem;
+        private ToolStripMenuItem textToolStripMenuItem;
+        private Label label1;
+    }
+}
---
+++ TextDisplay/Form1.cs
@@ -0,0 +1,264 @@
+using System.Text.Json;
+using Microsoft.VisualBasic;
+
+namespace TextDisplay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// フォームの移動
+        /// </summary>
+        /// <param name="eX"></param>
+        /// <param name="eY"></param>
+        private void FormMove(int eX, int eY)
+        {
+            // 移動先のフォーム位置
+            Rectangle newPosition =
+                new(Left + eX, Top + eY, Width, Height);
+
+            // 画面の端に吸着させて配置する
+            if (snapAssistToolStripMenuItem.Checked)
+            {
+                // 吸着するサイズ
+                Size gap = new(16, 16);
+
+                // 判定用のRECT
+                Rectangle newRect;
+
+                // 作業領域の取得（この作業領域の内側に吸着する）
+                Size area = new(
+                    Screen.FromControl(label1).WorkingArea.Width,
+                    Screen.FromControl(label1).WorkingArea.Height);
+
+                // 作業領域の左上隅の座標の取得
+                Point wp = Screen.FromControl(label1).WorkingArea.Location;
+
+                // 画面端の判定用（画面の端の位置に、吸着するサイズ分のRECTを定義する）
+                Rectangle rectLeft =
+                    new(wp.X, wp.Y, gap.Width, area.Height);
+                Rectangle rectTop =
+                    new(wp.X, wp.Y, area.Width, gap.Height);
+                Rectangle rectRight =
+                    new(area.Width - gap.Width + wp.X, wp.Y, gap.Width, area.Height);
+                Rectangle rectBottom =
+                    new(wp.X, area.Height - gap.Height + wp.Y, area.Width, gap.Height);
+
+                // 衝突判定
+                // 判定用のRECTを自分のウィンドウの隅に重ねるように移動し、
+                // 画面端の判定用のRECTと衝突しているかチェックする。
+                // 衝突していた場合は、吸着させるように移動する
+
+                // 左端衝突判定
+                {
+                    newRect = newPosition;
+                    newRect.Width = gap.Width;
+
+                    if (newRect.IntersectsWith(rectLeft))
+                    {
+                        // 左端に吸着させる
+                        newPosition.X = wp.X;
+                    }
+                }
+                // 右端衝突判定
+                {
+                    newRect = newPosition;
+                    newRect.X = newPosition.Right - gap.Width;  // ウィンドウの右隅
+                    newRect.Width = gap.Width;
+
+                    if (newRect.IntersectsWith(rectRight))
+                    {
+                        // 右端に吸着させる
+                        newPosition.X = area.Width - Width + wp.X;
+                    }
+                }
+                // 上端衝突判定
+                {
+                    newRect = newPosition;
+                    newRect.Height = gap.Height;
+
+                    if (newRect.IntersectsWith(rectTop))
+                    {
+                        // 上端に吸着させる
+                        newPosition.Y = wp.Y;
+                    }
+                }
+                // 下端衝突判定
+                {
+                    newRect = newPosition;
+                    newRect.Y = newPosition.Bottom - gap.Height; // ウィンドウの下端
+                    newRect.Height = gap.Height;
+
+                    if (newRect.IntersectsWith(rectBottom))
+                    {
+                        // 下端に吸着させる
+                        newPosition.Y = area.Height - Height + wp.Y;
+                    }
+                }
+            }
+
+            // 実際に移動させる
+            Left = newPosition.Left;
+            Top = newPosition.Top;
+        }
+
+        /// <summary>
+        /// フォームのサイズ変更
+        /// </summary>
+        private void FormSizeChange()
+        {
+            //文字列の大きさを計測してサイズ変更
+            Size size = TextRenderer.MeasureText(label1.Text, label1.Font);
+            if (size.Width == 0)
+            {
+                size.Width = 20;
+                size.Height= label1.Height;
+            }
+            Size = size;
+        }
+
+        ///マウスのクリック位置を記憶
+        private Point mousePoint;
+
+        ///Form1のMouseDownイベントハンドラ
+        ///マウスのボタンが押されたとき
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                //位置を記憶する
+                mousePoint = new Point(e.X, e.Y);
+            }
+        }
+
+        ///Form1のMouseMoveイベントハンドラ
+        ///マウスが動いたとき
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                FormMove(e.X - mousePoint.X, e.Y - mousePoint.Y);
+            }
+        }
+
+        private void SnapAssistToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            snapAssistToolStripMenuItem.Checked = !snapAssistToolStripMenuItem.Checked;
+        }
+
+        private void TopmostToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TopMost = !TopMost;
+            topmostToolStripMenuItem.Checked = TopMost;
+        }
+
+        private void FontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fontDialog1.Font = label1.Font;
+            fontDialog1.Color = label1.ForeColor;
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                label1.ForeColor = fontDialog1.Color;
+                label1.Font = fontDialog1.Font;
+                FormSizeChange();
+            }
+        }
+
+        private void ForeColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            colorDialog1.Color = label1.ForeColor;
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                label1.ForeColor = colorDialog1.Color;
+            }
+        }
+
+        private void BackColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            colorDialog1.Color = BackColor;
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                BackColor = colorDialog1.Color;
+                label1.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var _param = Deserialize<ParamTextBox?>("TextDisplay.config.json");
+            if (_param is not null)
+            {
+                param = _param;
+                label1.Text = param.Text;
+                label1.Font = new Font(param.Font.FontFamily, param.Font.Size);
+            }
+            FormSizeChange();
+        }
+
+        private void CloseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            param = new ParamTextBox
+            {
+                Text = label1.Text,
+                ForeColor = label1.ForeColor,
+                BackColor = label1.BackColor,
+                Font = new ParamTextFont
+                {
+                    FontFamily = label1.Font.FontFamily.Name,
+                    Size = label1.Font.Size,
+                },
+            };
+            Serialize(param, "TextDisplay.config.json");
+            Close();
+        }
+
+        private static void Serialize<T>(T data, string jsonfile)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            };
+
+            using var stream = new FileStream(jsonfile, FileMode.Create, FileAccess.Write);
+            JsonSerializer.SerializeAsync(stream, data, options);
+        }
+        private static T? Deserialize<T>(string jsonfile)
+        {
+            if (File.Exists(jsonfile))
+            {
+                var jsonString = File.ReadAllText(jsonfile);
+                return JsonSerializer.Deserialize<T>(jsonString);
+            }
+            return default;
+        }
+
+        private ParamTextBox param = new();
+
+        public class ParamTextBox
+        {
+            public string Text { get; set; } = "Right click to edit this message.";
+            public Color ForeColor { get; set; } = Color.White;
+            public Color BackColor { get; set; } = Color.MediumBlue;
+            public ParamTextFont Font { get; set; } = new ParamTextFont();
+        }
+        public class ParamTextFont
+        {
+            public string FontFamily { get; set; } = "Yu Gothic UI";
+            public float Size { get; set; } = 24;
+        }
+
+        private void TextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var str = Interaction.InputBox("", "Text", label1.Text);
+            if (str != "")
+            {
+                label1.Text = str;
+                FormSizeChange();
+            }
+        }
+    }
+}
